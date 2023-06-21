d25799d8ce4475027413020c67c7263fb85bc142
arelyquinonez <quinonez00@hotmail.com>
2023-06-21 13:42:45 -0600

    home

--- app/(webpage)/page.js
+++ app/(webpage)/page.js
@@ -3,7 +3,148 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      Pagina de home
+      {/* <!-- Hero Sextion --> */}
+      <div className="relative overflow-hidden bg-no-repeat bg-cover">
+        <div id="hero" className="max-h-[80vh]">
+          <video id="hero-video" playsinline autoplay muted width="x" height="x" />
+        </div>
+      </div>
+      {/* <!-- Hero Section --> */}
+
+
+      {/* <!-- Higuera Golf Club --> */}
+      <div id="about-us" className="flex flex-row items-center mt-20 mb-0 md:mb-0 md:mt-32 ">
+        <div className="flex items-center justify-center w-full md:w-2/5">
+          <div className="text-center md:text-left w-full md:max-w-screen-sm mx-auto  bg-white px-5 py-10 md:pr-10">
+            <h1 className="text-5xl text-main">Higuera Golf Club</h1><br />
+            <p className="text-justify text-xl mt-5">Located in Punta de Mita, México, Higuera Golf Club offers world className golf with
+              innovative services
+              and amenities. This Greg Norman designed course features some of the most breathtaking scenery available
+              with stunning oceanfront features, well-manicured fairways and greens that seamlessly blend into the
+              surrounding beauty.<br />
+            </p>
+            {/* <Image classNameName='float-right my-10 md:hidden' src={'/img/higuera-golf-club-1.jpg'} width={1000} height={600} /> */}
+            <div className="mt-10">
+              <a className="text-lg text-white bg-main px-5 py-2 rounded-sm hidden" href="#">WATCH VIDEO</a>
+            </div>
+          </div>
+        </div>
+        <div className="w-3/5 hidden md:block ">
+          <Image className='float-right' src={'/img/higuera-golf-club-1.jpg'} width={1000} height={600} />
+        </div>
+      </div>
+      {/* <!-- Higuera Golf Club-end --> */}
+
+
+      {/* <!-- bienvenido --> */}
+      <div className="mb-10 md:mb-26 md:mt-5" >
+        <div className="flex flex-wrap items-center max-w-screen-xl mx-auto ">
+          <div className="text-center px-5 w-full my-14 md:my-20 sm:w-1/2 md:w-1/4">
+            <i className="fa-solid fa-golf-ball-tee icono_HGC"></i>
+            <h3 className="text-center text-2xl text-main ">TEE TIMES</h3>
+            <p className="text-center text-xl my-5">Open daily, booking available from 7:30 am to 3:00 pm.</p>
+            <div className="mt-8">
+              <a className="text-white bg-main px-3 py-2 rounded-sm" href="https://www.chronogolf.com/club/higuera-golf-club"> BOOK A TEE TIME <i className="fa-solid fa-calendar text-white pl-3"></i></a>
+            </div>
+          </div>
+
+          <div className="text-center px-5 w-full my-14 md:my-20 sm:w-1/2 md:w-1/4">
+            <i className="fa-solid fa-trophy icono_HGC"></i>
+            <h3 className="text-center text-2xl text-main ">COURSE EXPERIENCE</h3>
+            <p className="text-center text-xl my-5">Check out the hole layouts, bunkers, distance and the incredible views.</p>
+            <div className="mt-8">
+              <a className="text-white bg-[#008000] px-3 py-2 rounded-sm" href="course-experience.html"> VIDEOS HOLE BY HOLE <i className="fa-solid fa-golf-ball-tee text-white pl-3"></i></a>
+            </div>
+          </div>
+
+          <div className="text-center px-5 w-full my-14 md:my-20 sm:w-1/2 md:w-1/4">
+            <i className="fa-solid fa-martini-glass-citrus  icono_HGC"></i>
+            <h3 className="text-center text-2xl text-main ">FOOD & BEVERAGE</h3>
+            <p className="text-center text-xl my-5">Experience our 2 snack bars on the course and our incredible restaurant.</p>
+            <div id="open-popup-2" className="mt-8">
+              <a className="text-white bg-main px-3 py-2 rounded-sm" href="#">SEE GALLERY <i className="fa-solid fa-image text-white pl-3"></i></a>
+            </div>
+          </div>
+
+          <div className="text-center px-5 w-full my-14 md:my-20 sm:w-1/2 md:w-1/4">
+            <i className="fa-solid fa-book  icono_HGC"></i>
+            <h3 className="text-center text-2xl text-main ">SCORECARD</h3>
+            <p className="text-center text-xl my-5">Check out the distance between holes before your tee time.</p>
+            <div className="mt-8">
+              <a className="text-white bg-main px-3 py-2 rounded-sm" href="img/scorecard.jpg" target="_blank" download>DOWNLOAD <i className="fa-solid fa-circle-down text-white pl-3"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* <!--Galeria--> */}
+      <div className="galeria max-h-[800px] overflow-hidden	">
+      <Image src={'/img/higuera-golf-club-2.jpg'} width={0} height={800} />
+      <Image src={'/img/higuera-golf-club-3.jpg'} width={0} height={800} />
+      <Image src={'/img/higuera-golf-club-4.jpg'} width={0} height={800} />
+      <Image src={'/img/higuera-golf-club-5.jpg'} width={0} height={800} />
+      </div>
+      {/* <!--Galeria-end--> */}
+
+
+      {/* <!--Golf Course--> */}
+      <div className="flex flex-col items-center md:flex-row mb-20 mt-32 max-w-screen-xl mx-auto">
+        <div className="w-full md:w-1/2 lg:w-2/5 flex items-center justify-center hidden md:block">
+          <Image className='object-fill' src={'/img/6.jpg'} width={400} height={750} />
+        </div>
+        <div className="flex items-center justify-center w-full text-center md:text-left md:w-1/2 lg:w-3/5">
+          <div className="max-w-screen-sm mx-auto p-5">
+            <h1 className="text-5xl text-main">The Golf Course</h1><br />
+            <p className="text-justify text-xl mb-5">The facilities and amenities at Higuera Golf Club are world className,
+              an 18-hole Par 72 masterpiece artfully routed through the magnificent natural landscape.</p>
+            <p className="text-justify text-xl mb-5">Just steps away from the beachfront, Higuera Golf Club challenges golfers with tricky layouts, strategic bunkering,
+              ridges above the beach,  amazing ocean views spread throughout the course, and even a peninsula green that's surrounded
+              by water on three sides.</p>
+            <p className="text-justify text-xl mb-5"> Complement your experience by visiting our Pro-Shop & Snack Bar.</p>
+            <div className="mt-10">
+              <a className="text-lg text-white bg-main px-5 py-2 rounded-sm" href="course-experience.html" target="_blank">SEE OUR COURSE EXPERIENCE</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!--Golf Course end--> */}
+      <div className="my-32 uppercase max-w-screen-md mx-auto">
+        <p className="text-center text-5xl text-main">Higuera Golf Club is truly one of the best golf experiences in all of México.</p>
+      </div>
+      {/* <!--Memberships, tournaments & events --> */}
+      <div className="flex max-w-screen-xl mx-auto justify-around my-20 flex-wrap relative">
+        <div id="open-popup-1">
+          <Image className='p-4 my-10' src={'/img/2.jpg'} width={400} height={700} />
+          <a className="bg-main">
+            <div className="text-center -mt-40  my-3 mx-14 py-2 sticky border-[1px] border-solid rounded-sm transition-all hover:bg-main ">
+              <h2 className="text-white text-2xl ">MEMBERSHIP</h2>
+              <p className="text-white text-xl">REWARDING</p>
+            </div>
+          </a>
+        </div>
+        <div>
+        <Image className='p-4 my-10' src={'/img/3.jpg'} width={400} height={700} />
+          <a href="/halloweencup/">
+            <div className="text-center -mt-40   my-3 mx-14 py-2 sticky border-[1px] border-solid transition-all hover:bg-main">
+              <h2 className="text-white text-2xl ">CHAMPIONSHIP GOLF</h2>
+              <p className="text-white text-xl">LEGENDARY</p>
+            </div>
+          </a>
+        </div>
+        {/* <!-- 
+    <div id="open-popup-3">
+      <img className="p-4 my-10" src="img/5.jpg" alt="">
+      <a href="#">
+        <div className="text-center -mt-40   my-3 mx-14 py-2 sticky border-[1px] border-solid transition-all hover:bg-main ">
+          <h2 className="text-white text-2xl ">WEEDINGS & EVENTS</h2>
+          <p className="text-white text-xl">UNFORGETTABLE</p>
+        </div>
+      </a>
+    </div>--> */}
+      </div>
+
+
 
     </main>
   )
--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,9 +1,98 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <nav>
-        Header
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Higuera Golf Club | Higuera Golf Club is truly one of the best golf experiences in all of México</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+          integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+
+        {/* <!--Google Fonts--> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@700&family=Nunito:wght@200&display=swap" rel="stylesheet" />
+
+        {/* <!--favicon--> */}
+        <link rel="icon" href="/img/favicon.png" type="image/png" />
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3TSYV67NNL"></script>
+      </head>
+
+      <header>
+        <div className="float-right mx-4 pt-2">
+          <a href="es.html">ESPAÑOL</a> | <a href="/">ENGLISH</a>
+        </div>
+        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+          <div className="px-6 w-full flex flex-wrap items-center justify-between">
+          <Image src={'/img/higuera-golf-club-logo.webp'} width={150} height={600}/>
+            <div className="flex items-center">
+              <button
+                className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
+                type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentY"
+                aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="currentColor"
+                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+                  </path>
+                </svg>
+              </button>
+            </div>
+            
+            {/* <!-- collapse --> */}
+            <div className="navbar-collapse  grow items-center menu-mobile transition-all lg:block"
+              id="navbarSupportedContentY">
+              <ul className="navbar-nav mr-auto lg:flex lg:flex-row lg:float-right">
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-4 py-2 transition duration-150 ease-in-out"
+                    href="#about-us" data-mdb-ripple="true" data-mdb-ripple-color="light">ABOUT US</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-4 py-2 transition duration-150 ease-in-out"
+                    href="course-experience.html" data-mdb-ripple="true"
+                    data-mdb-ripple-color="light">COURSE
+                    EXPERIENCE</a>
+                </li>
+                <li className="nav-item mb-2 lg:mb-0">
+                  <a className="nav-link block pr-2 lg:px-4 py-2 transition duration-150 ease-in-out"
+                    href="#contact" data-mdb-ripple="true" data-mdb-ripple-color="light">CONTACT</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-4 py-2 bg-main rounded-sm text-white hover:text-gray-700 transition duration-150 ease-in-out"
+                    href="https://www.chronogolf.com/club/higuera-golf-club" data-mdb-ripple="true"
+                    data-mdb-ripple-color="light">BOOK A TEE TIME</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-4 py-2  transition duration-150 ease-in-out"
+                    target="_blank"
+                    href="https://www.facebook.com/Higuera-Golf-Club-108411901636841/?ref=page_internal"
+                    data-mdb-ripple="true" data-mdb-ripple-color="light"><i
+                      className="fa-brands fa-facebook"></i></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-4 py-2 transition duration-150 ease-in-out"
+                    target="_blank" href="https://www.instagram.com/higueragolfclub/" data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"><i className="fa-brands fa-instagram"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
     </nav>
   )
 }
